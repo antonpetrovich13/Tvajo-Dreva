941361d874650a0e3ab524a5b594f48c3e9a10c7
Anton Petrovich <63902821+antonpetrovich13@users.noreply.github.com>
2021-01-15 19:52:03 +0300

    Add files via upload

---
+++ #source/js/script.js
@@ -0,0 +1,96 @@
+"use strict"
+
+// МЕНЮ-БУРГЕР
+$(document).ready(function () {
+	$('.header__burger').click(function (event) {
+		$('.header__burger,.header__menu').
+			toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
+
+$(document).ready(function () {
+	$('.header__link').click(function (event) {
+		$('.header__burger, .header__menu').removeClass('active');
+		$('body').removeClass('lock')
+	});
+});
+
+
+
+// ПАРАЛАКС
+$(window).scroll(function (event) {
+	var s = 0 - $(this).scrollTop() / 2;
+	$('.main__mainscreen').css('transform', 'translate3d(0, ' + s + 'px, 0)');
+});
+
+
+
+// ПЛАВНАЯ НАВИГАЦИЯ
+$(function () {
+	$("a[href^='#']").click(function () {
+		var _href = $(this).attr("href");
+		var fixed_offset = 50;
+		$("html, body").animate({ scrollTop: $(_href).offset().top - fixed_offset }, 500);
+		return false;
+	});
+});
+
+
+$(function () {
+	$('.footer__logo').click(function () {
+		$('html, body').animate({ scrollTop: 0 }, 'slow');
+	});
+});
+
+
+// СЛАЙДЕР
+$(window).on('load resize', function () {
+	if ($(window).width() < 600) {
+		$('#slider:not(.slick-initialized)').slick({
+			dots: true,
+			arrows: false,
+			infinite: true,
+			speed: 500,
+			cssEase: 'ease-out',
+			touchThreshold: 10,
+			adaptiveHeight: true,
+			slidesToShow: 1,
+		});
+	} else {
+		$("#slider.slick-initialized").slick("unslick");
+	}
+});
+
+
+
+//ПЕРЕДАЧА НАЗВАНИЯ В ФОРМУ
+
+//ПЕРЕДАЧА НАЗВАНИЯ В ФОРМУ
+/*$(document).ready(function () {
+	$('.earrings__item').click(function (event) {
+		var local = document.querySelector(".earrings__label").innerText;
+		localStorage.setItem("productLabel", local);
+	})
+})*/
+
+
+
+document.querySelectorAll(".earrings__item").forEach(function (el) {
+	el.addEventListener("click", function (ev) {
+		if (ev.target != el) {
+			var local = el.querySelector(".earrings__label").innerText;
+			localStorage.setItem("productLabel", local);
+		}
+	});
+})
+
+
+if (document.querySelector(".form__input_data")) {
+	document.querySelector(".form__input_data").value = localStorage.getItem("productLabel");
+}
+
+
+
+
+
