d47cbed1ef4c622fb789d59e1754538e76f7120e
Anton Petrovich <63902821+antonpetrovich13@users.noreply.github.com>
2021-02-05 21:47:51 +0300

    Add files via upload

--- #source/js/script.js
+++ #source/js/script.js
@@ -1,96 +1,82 @@
+"use strict"
 
-// СЛАЙДЕР (EASY)
+// МЕНЮ-БУРГЕР
+$(document).ready(function () {
+	$('.header__burger').click(function (event) {
+		$('.header__burger,.header__menu').
+			toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
+
+$(document).ready(function () {
+	$('.header__link').click(function (event) {
+		$('.header__burger, .header__menu').removeClass('active');
+		$('body').removeClass('lock')
+	});
+});
+
+
+
+// ПАРАЛАКС
+$(window).scroll(function (event) {
+	var s = 0 - $(this).scrollTop() / 2;
+	$('.main__mainscreen').css('transform', 'translate3d(0, ' + s + 'px, 0)');
+});
+
+
+
+// ПЛАВНАЯ НАВИГАЦИЯ
+$(function () {
+	$("a[href^='#']").click(function () {
+		var _href = $(this).attr("href");
+		var fixed_offset = 50;
+		$("html, body").animate({ scrollTop: $(_href).offset().top - fixed_offset }, 500);
+		return false;
+	});
+});
+
+
+$(function () {
+	$('.footer__logo').click(function () {
+		$('html, body').animate({ scrollTop: 0 }, 'slow');
+	});
+});
+
+
+// СЛАЙДЕР
 $(window).on('load resize', function () {
-	if ($(window).width() < 950) {
+	if ($(window).width() < 600) {
 		$('#slider:not(.slick-initialized)').slick({
-			dots: false,
-			arrows: true,
+			dots: true,
+			arrows: false,
 			infinite: true,
 			speed: 500,
 			cssEase: 'ease-out',
 			touchThreshold: 10,
 			adaptiveHeight: true,
-			slidesToShow: 2,
-			responsive: [
-				{
-					breakpoint: 650,
-					settings: {
-						slidesToShow: 1,
-					}
-				}
-			]
+			slidesToShow: 1,
 		});
 	} else {
 		$("#slider.slick-initialized").slick("unslick");
 	}
 });
 
-// СЛАЙДЕР (PRODUCTS)
-$(document).ready(function () {
-	$('.products__slider').slick({
-		arrows: false,
-		dots: false,
-		speed: 300,
-		infinite: true,
-		slidesToShow: 9,
-		centerMode: true,
-		touchThreshold: 40,
-		cssEase: 'ease-out',
-		responsive: [
-			{
-				breakpoint: 1380,
-				settings: {
-					slidesToShow: 8,
-				}
-			}, {
-				breakpoint: 1230,
-				settings: {
-					slidesToShow: 7,
-				}
-			},
-			{
-				breakpoint: 1080,
-				settings: {
-					slidesToShow: 6,
-				}
-			},
-			{
-				breakpoint: 950,
-				settings: {
-					slidesToShow: 5,
-				}
-			},
-			{
-				breakpoint: 810,
-				settings: {
-					slidesToShow: 4,
-				}
-			},
-			{
-				breakpoint: 670,
-				settings: {
-					slidesToShow: 3,
-					centerMode: false,
-					dots: true,
-				}
-			},
-			{
-				breakpoint: 530,
-				settings: {
-					slidesToShow: 2,
-					centerMode: false,
-					dots: true,
-				}
-			},
-			{
-				breakpoint: 390,
-				settings: {
-					slidesToShow: 1,
-					centerMode: false,
-					dots: true,
-				}
-			},
-		]
+
+
+//ПЕРЕДАЧА НАЗВАНИЯ В ФОРМУ
+document.querySelectorAll(".earrings__column").forEach(function (el) {
+	el.addEventListener("click", function (ev) {
+		if (ev.target != el) {
+			var local = el.querySelector(".earrings__label").innerText;
+			localStorage.setItem("productLabel", local);
+		}
 	});
-});
+})
+
+if (document.querySelector(".form__input_data")) {
+	document.querySelector(".form__input_data").value = localStorage.getItem("productLabel");
+}
+
+
 
